ae66c9469f51808698621f7911faf2e35d8e17df
Milan Kotykov <mk@allbridge.io>
2023-09-20 11:54:41 +0300

    fix: update CCTP api url

--- scripts/cctp-bridge/receive-tokens.ts
+++ scripts/cctp-bridge/receive-tokens.ts
@@ -2,9 +2,8 @@ import { ethers } from 'hardhat';
 import { getEnv, handleTransactionResult, } from '../helper';
 
 const extraGasAmount = '1000';
-
-const sentMessage = '0x000000000000000000000003000000000003965b000000000000000000000000d0c3da58f55358142b8d3e06c1c30c5c6114efe800000000000000000000000012dcfd3fe2e9eac2859fd1ed86d2ab8c5a2f935200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000007865c6e87b9f70255377e024ace6630c1eaa37f0000000000000000000000009aca1c932640a5743b777162d6d3c6196053596e000000000000000000000000000000000000000000000000000000000001fbd00000000000000000000000005232fca9c8642b03036c5246718a5ba458641e2a';
-const messageHash = ethers.utils.keccak256(sentMessage);
+// get sentMessage by running the script `get-sent-message.ts` on the source chain
+const sentMessage = '0x000000000000000000000003000000000003965c000000000000000000000000d0c3da58f55358142b8d3e06c1c30c5c6114efe800000000000000000000000012dcfd3fe2e9eac2859fd1ed86d2ab8c5a2f935200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000007865c6e87b9f70255377e024ace6630c1eaa37f0000000000000000000000009aca1c932640a5743b777162d6d3c6196053596e0000000000000000000000000000000000000000000000000000000000030d400000000000000000000000005232fca9c8642b03036c5246718a5ba458641e2a';
 const isTestnet = true;
 
 async function main() {
@@ -13,6 +12,7 @@ async function main() {
 
   const cctpBridge = await ethers.getContractAt('CctpBridge', cctpBridgeAddress);
 
+  const messageHash = ethers.utils.keccak256(sentMessage);
   const signature = await getSignature(messageHash);
   console.log('signature:', signature);
 
@@ -28,11 +28,12 @@ async function main() {
 async function getSignature(messageHash: string) {
   let attestationResponse = { status: '', attestation: '' };
   while (attestationResponse?.status !== 'complete') {
-    const irisUrl = isTestnet ? 'https://iris-api-sandbox.circle.com' : 'https://api.circle.com';
+    console.log('Request signature...');
+    const irisUrl = isTestnet ? 'https://iris-api-sandbox.circle.com' : 'https://iris-api.circle.com';
     const response = await fetch(`${irisUrl}/attestations/${messageHash}`);
     attestationResponse = await response.json();
 
-    console.log('attestationResponse=', JSON.stringify(attestationResponse, null, 2));
+    console.log('attestationResponse:', JSON.stringify(attestationResponse, null, 2));
     await new Promise((resolve) => setTimeout(resolve, 2000));
   }
 
