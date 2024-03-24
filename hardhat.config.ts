6458aff1ea4fac5a4ef01734b99200a156764f10
Yaroslav Nekryach <yaroslavnekryach@gmail.com>
2024-03-24 19:05:56 +0200

    Fixed linter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -74,7 +74,7 @@ const config: HardhatUserConfig = {
     },
   ],
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
   warnings: {
     '*': {
