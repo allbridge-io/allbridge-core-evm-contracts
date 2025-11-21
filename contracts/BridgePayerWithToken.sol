e84f0666eef7d94a114a7fa2d04ef0f3f57bb218
Milan Kotykov <mk@allbridge.io>
2025-11-21 06:52:43 -0300

    refactor: remove MockBridge

--- contracts/BridgePayerWithToken.sol
+++ contracts/BridgePayerWithToken.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {MockBridge} from "./test/MockBridge.sol";
 import {GasUsage} from "./GasUsage.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
