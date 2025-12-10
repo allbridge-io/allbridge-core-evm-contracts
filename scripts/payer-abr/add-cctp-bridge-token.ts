76b6fdea0fcbc2574eb2bfb9aa49f48cf2b614e7
Milan Kotykov <mk@allbridge.io>
2025-12-10 09:24:27 -0300

    chore: add script `register-target`

--- scripts/payer-abr/add-cctp-bridge-token.ts
+++ scripts/payer-abr/add-cctp-bridge-token.ts
@@ -5,6 +5,7 @@ async function main() {
   const contractAddress = getEnv('ABR_PAYER_ADDRESS');
   const tokenAddress = getEnv('USDC_ADDRESS');
   const bridgeAddress = getEnv('CCTP_BRIDGE_ADDRESS');
+  // const bridgeAddress = getEnv('CCTP_V2_BRIDGE_ADDRESS');
 
   const contract = await ethers.getContractAt('PayerWithAbr', contractAddress);
   const result = await contract.approveBridgeToken(
---
+++ scripts/payer-abr/register-target.ts
@@ -0,0 +1,30 @@
+import { ethers } from 'hardhat';
+import { getEnv, handleTransactionResult } from '../helper';
+
+const map = new Map<string, [number, string, string]>();
+map.set('Allbridge', [1, getEnv('BRIDGE_ADDRESS'), 'swapAndBridge(bytes32,uint256,bytes32,uint256,bytes32,uint256,uint8,uint256)']);
+map.set('Wormhole', [2, getEnv('BRIDGE_ADDRESS'), 'swapAndBridge(bytes32,uint256,bytes32,uint256,bytes32,uint256,uint8,uint256)']);
+map.set('CCTP', [3, getEnv('CCTP_BRIDGE_ADDRESS'), 'bridge(uint256,bytes32,uint256,uint256)']);
+map.set('CCTPv2', [4, getEnv('CCTP_V2_BRIDGE_ADDRESS'), 'bridge(uint256,bytes32,uint256,uint256)']);
+map.set('OFT', [5, getEnv('OFT_BRIDGE_ADDRESS'), 'bridge(address,uint256,bytes32,uint256,uint256,uint256,uint256)']);
+
+async function main() {
+  const contractAddress = getEnv('ABR_PAYER_ADDRESS');
+  const contract = await ethers.getContractAt('PayerWithAbr', contractAddress);
+  for (const [name, entry] of map) {
+    const [messengerProtocol, targetAddress, functionSignature] = entry;
+    if (!targetAddress) continue;
+    console.log(`Register target ${name} with params: protocol ${messengerProtocol}, address ${targetAddress}, function ${functionSignature}`);
+    const result = await contract.registerTarget(
+      messengerProtocol,
+      targetAddress,
+      functionSignature,
+    );
+    await handleTransactionResult(result);
+  }
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
