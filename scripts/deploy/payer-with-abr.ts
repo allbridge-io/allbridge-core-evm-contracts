14c15ca7c083394714bb02745e18103d22050749
Milan Kotykov <mk@allbridge.io>
2025-12-11 12:29:01 -0300

    chore: update `PayerWithAbr` scripts

--- scripts/deploy/payer-with-abr.ts
+++ scripts/deploy/payer-with-abr.ts
@@ -3,18 +3,19 @@ import { getEnv, handleDeployResult } from '../helper';
 // @ts-ignore
 import { loadSolSource, assertContainsSafeERC20 } from '../utils/code-asserts';
 
-const CHAIN_PRECISION = 18;
-
 async function main() {
   const source = loadSolSource('PayerWithAbr');
   assertContainsSafeERC20(source);
 
   const abrTokenAddress = getEnv('ABR_TOKEN_ADDRESS');
+  const gasOracle = getEnv('GAS_ORACLE_ADDRESS');
+  const chainId = getEnv('CHAIN_ID');
 
   const Contract = await ethers.getContractFactory('PayerWithAbr');
   const contract = await Contract.deploy(
     abrTokenAddress,
-    CHAIN_PRECISION,
+    gasOracle,
+    chainId,
   );
 
   await handleDeployResult(contract);
--- scripts/payer-abr/set-exchange-rate.ts
+++ scripts/payer-abr/set-exchange-rate.ts
@@ -2,18 +2,17 @@ import { ethers } from 'hardhat';
 import { getEnv, handleTransactionResult } from '../helper';
 import { formatUnits, parseUnits } from 'ethers/lib/utils';
 
-const EXCHANGE_RATE_PRECISION = 18;
-
 async function main() {
   const contractAddress = getEnv('ABR_PAYER_ADDRESS');
   const contract = await ethers.getContractAt('PayerWithAbr', contractAddress);
+  const exchangeRatePrecision = Number(await contract.EXCHANGE_RATE_PRECISION());
 
-  const rateDecimal = getEnv('ABR_EXCHANGE_RATE'); // ABR/ETH
+  const rateDecimal = getEnv('ABR_PRICE'); // ABR/USD
   const currentExchangeRate = await contract.exchangeRate();
-  console.log('Current rate:', formatUnits(currentExchangeRate.toString(), EXCHANGE_RATE_PRECISION));
+  console.log('Current rate:', formatUnits(currentExchangeRate.toString(), exchangeRatePrecision));
   console.log('New rate:', rateDecimal);
 
-  const result = await contract.setExchangeRate(parseUnits(rateDecimal, EXCHANGE_RATE_PRECISION));
+  const result = await contract.setExchangeRate(parseUnits(rateDecimal, exchangeRatePrecision));
   await handleTransactionResult(result);
 }
 
