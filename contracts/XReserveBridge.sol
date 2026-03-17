e149cccb1e7c808effd0ee4b4f21c453519a1b32
Yaroslav Nekryach <yaroslavnekryach@gmail.com>
2026-03-17 14:13:10 +0200

    Add bridge registration and scripting functionality for xReserveBridge

--- contracts/XReserveBridge.sol
+++ contracts/XReserveBridge.sol
@@ -4,44 +4,22 @@ pragma solidity ^0.8.18;
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {IGasOracle} from "./interfaces/IGasOracle.sol";
 import {IXReserve} from "./interfaces/xreserve/IXReserve.sol";
-import {GasUsage} from "./GasUsage.sol";
 
-contract XReserveBridge is GasUsage {
+contract XReserveBridge is Ownable {
     using SafeERC20 for IERC20Metadata;
 
-    uint internal constant ORACLE_PRECISION = 18;
     uint internal constant BP = 1e4;
     uint internal constant MAX_FEE_SHARE_P = 1e9;
 
     uint public immutable chainId;
     // Admin fee share (in basis points)
     uint public adminFeeShareBP;
-    uint public maxFeeShare = 100000;
+    uint public maxFeeShare = 0;
     IERC20Metadata private immutable token;
     IXReserve private immutable xReserve;
-    // precomputed value of the scaling factor required for converting the stable token to gas amount
-    uint private immutable stableTokensForGasScalingFactor;
-    // precomputed value to divide by to change the precision from the Gas Oracle precision to the stable token precision
-    uint private immutable fromGasOracleScalingFactor;
 
     mapping(uint chainId => uint domainNumber) private chainIdDomainMap;
-
-    /**
-     * @notice Emitted when the contract receives some gas directly.
-     */
-    event ReceivedGas(address sender, uint amount);
-
-    /**
-     * @notice Emitted when the contract receives message
-     */
-    event ReceivedMessageId(bytes32 messageId);
-
-    /**
-     * @notice Emitted when the contract sends some extra gas to the recipient of tokens.
-     */
-    event ReceivedExtraGas(address recipient, uint amount);
 
     /**
      * @notice Emitted when tokens are sent on the source blockchain.
@@ -51,10 +29,6 @@ contract XReserveBridge is GasUsage {
         bytes32 recipient,
         uint amount,
         uint destinationChainId,
-        uint receivedRelayerFeeFromGas,
-        uint receivedRelayerFeeFromTokens,
-        uint relayerFee,
-        uint receivedRelayerFeeTokenAmount,
         uint adminFeeTokenAmount,
         uint maxFee
     );
@@ -63,43 +37,33 @@ contract XReserveBridge is GasUsage {
 
     constructor(
         uint chainId_,
-        uint chainPrecision_,
         address tokenAddress,
-        address xReserve_,
-        IGasOracle gasOracle_
-    ) GasUsage(gasOracle_) {
+        address xReserve_
+    ) {
         chainId = chainId_;
         token = IERC20Metadata(tokenAddress);
-        uint tokenDecimals = token.decimals();
         xReserve = IXReserve(xReserve_);
         token.approve(xReserve_, type(uint256).max);
-        stableTokensForGasScalingFactor = 10 ** (ORACLE_PRECISION - tokenDecimals + chainPrecision_);
-        fromGasOracleScalingFactor = 10 ** (ORACLE_PRECISION - tokenDecimals);
     }
 
     /**
      * @notice Initiates a bridging process of the token to another blockchain via xReserve.
-     * @param amount The amount of tokens to send (including `relayerFeeTokenAmount`).
+     * @param amount The amount of tokens to send.
      * @param recipient The recipient address.
      * @param destinationChainId The ID of the destination chain.
-     * @param relayerFeeTokenAmount The amount of tokens to be deducted from the transferred amount as a bridging fee.
      */
     function bridge(
         uint amount,
         bytes32 recipient,
-        uint destinationChainId,
-        uint relayerFeeTokenAmount
-    ) public payable {
-        require(amount > relayerFeeTokenAmount, "XRB: Amount <= relayer fee");
+        uint destinationChainId
+    ) public {
+        require(amount > 0, "XRB: Amount must be greater than zero");
         require(recipient != 0, "XRB: Recipient must be nonzero");
         token.safeTransferFrom(msg.sender, address(this), amount);
-        uint gasFromStables = _getStableTokensValueInGas(relayerFeeTokenAmount);
-        uint relayerFee = this.getTransactionCost(destinationChainId);
-        require(msg.value + gasFromStables >= relayerFee, "XRB: Not enough fee");
-        uint amountToSend = amount - relayerFeeTokenAmount;
+        uint amountToSend = amount;
         uint adminFee;
         if (adminFeeShareBP != 0) {
-            adminFee = (amountToSend * adminFeeShareBP) / BP;
+            adminFee = (amount * adminFeeShareBP) / BP;
             if (adminFee == 0) {
                 adminFee = 1;
             }
@@ -120,10 +84,6 @@ contract XReserveBridge is GasUsage {
             recipient,
             amountToSend,
             destinationChainId,
-            msg.value,
-            gasFromStables,
-            relayerFee,
-            relayerFeeTokenAmount,
             adminFee,
             maxFee
         );
@@ -137,37 +97,11 @@ contract XReserveBridge is GasUsage {
         uint amount,
         bytes32 recipient,
         bytes32 recipientWalletAddress,
-        uint destinationChainId,
-        uint relayerFeeTokenAmount
-    ) external payable {
-        bridge(amount, recipient, destinationChainId, relayerFeeTokenAmount);
+        uint destinationChainId
+    ) external {
+        bridge(amount, recipient, destinationChainId);
 
         emit TokensSentExtras(recipientWalletAddress);
-    }
-
-    /**
-     * @notice Completes the bridging process by sending the tokens on the destination blockchain.
-     * @param recipient The recipient address.
-     * @param messageId The message id to connect sent and received transaction (Sent tx id)
-     * @param message The message information.
-     * @param signature Concatenated 65-byte signature(s) of `message`.
-     */
-    function receiveTokens(
-        address recipient,
-        bytes32 messageId,
-        bytes calldata message,
-        bytes calldata signature
-    ) external payable {
-        xReserve.withdraw(message, signature);
-        // pass extra gas to the recipient
-        if (msg.value > 0) {
-            (bool sent, ) = payable(recipient).call{value: msg.value}("");
-            if (sent) {
-                emit ReceivedExtraGas(recipient, msg.value);
-            }
-        }
-
-        emit ReceivedMessageId(messageId);
     }
 
     /**
@@ -188,14 +122,7 @@ contract XReserveBridge is GasUsage {
     }
 
     /**
-     * @notice Allows the admin to withdraw the relayer fee collected in gas tokens.
-     */
-    function withdrawGas(uint amount) external onlyOwner {
-        payable(msg.sender).transfer(amount);
-    }
-
-    /**
-     * @notice Allows the admin to withdraw the admin fee and relayer fee collected in tokens.
+     * @notice Allows the admin to withdraw the admin fee collected in tokens.
      */
     function withdrawFeeInTokens() external onlyOwner {
         uint toWithdraw = token.balanceOf(address(this));
@@ -220,31 +147,10 @@ contract XReserveBridge is GasUsage {
         maxFeeShare = maxFeeShare_;
     }
 
-    /**
-     * @notice Calculates the amount of bridging fee nominated in the stable token.
-     * @param destinationChainId The ID of the destination chain.
-     * @return The total price of bridging.
-     */
-    function getBridgingCostInTokens(uint destinationChainId) external view returns (uint) {
-        return
-            gasOracle.getTransactionGasCostInUSD(destinationChainId, gasUsage[destinationChainId]) /
-            fromGasOracleScalingFactor;
-    }
-
     function getDomainByChainId(uint chainId_) public view returns (uint32) {
         uint domainNumber = chainIdDomainMap[chainId_];
         require(domainNumber > 0, "XRB: Unknown chain id");
         return uint32(domainNumber);
-    }
-
-    /**
-     * @notice Calculates the amount of gas equivalent in value to provided amount of tokens.
-     * @param stableTokenAmount The amount of tokens.
-     * @return amount of gas tokens.
-     */
-    function _getStableTokensValueInGas(uint stableTokenAmount) internal view returns (uint) {
-        if (stableTokenAmount == 0) return 0;
-        return (stableTokensForGasScalingFactor * stableTokenAmount) / gasOracle.price(chainId);
     }
 
     fallback() external payable {
@@ -252,6 +158,6 @@ contract XReserveBridge is GasUsage {
     }
 
     receive() external payable {
-        emit ReceivedGas(msg.sender, msg.value);
+        revert("Direct native token deposits not supported");
     }
 }
---
+++ scripts/xreserve-bridge/bridge.ts
@@ -0,0 +1,50 @@
+import { ethers } from 'hardhat';
+import {
+  addressToBytes32, getEnv,
+  handleTransactionResult,
+} from '../helper';
+import { parseEther, parseUnits } from 'ethers/lib/utils';
+
+const destinationChainId = 16;
+const totalTokens = process.env.TOTAL_TOKENS || '5';
+const recipient = '0x1ad8984e1d33e1ff33dc04d22af791c101c2a12e18';
+
+async function main() {
+  const xReserveBridgeAddress = getEnv('X_RESERVE_BRIDGE_ADDRESS');
+  const tokenAddress = getEnv('USDC_ADDRESS');
+
+  const signer = (await ethers.getSigners())[0];
+
+  const xReserveBridge = await ethers.getContractAt('XReserveBridge', xReserveBridgeAddress);
+
+  console.log('To send ', JSON.stringify({
+    totalTokens: totalTokens,
+  }, null, 2));
+
+  const token = await ethers.getContractAt(
+    '@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20',
+    tokenAddress,
+  );
+  const tokenDecimals = await token.decimals();
+  const totalTokensAmount = parseUnits(totalTokens, tokenDecimals);
+
+  // approve XReserve bridge
+  if ((await token.allowance(signer.address, xReserveBridge.address)).lt(totalTokensAmount)) {
+    console.log('Approve XReserve Bridge');
+    await handleTransactionResult(
+      await token.approve(xReserveBridge.address, ethers.constants.MaxUint256),
+    );
+  }
+
+  const result = await xReserveBridge.bridge(
+    totalTokensAmount,
+    addressToBytes32(recipient),
+    destinationChainId,
+  );
+  await handleTransactionResult(result);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
