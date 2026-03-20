5bd92243721a2265dc631ba8588eeb40662e5cc5
Yaroslav Nekryach <yaroslavnekryach@gmail.com>
2026-03-20 12:04:16 +0200

    Introduce `canLocalSwap` mechanism in Router and update related tests

--- contracts/interfaces/IRouter.sol
+++ contracts/interfaces/IRouter.sol
@@ -6,5 +6,7 @@ import {MessengerProtocol} from "./IBridge.sol";
 interface IRouter {
     function canSwap() external view returns (uint8);
 
+    function canLocalSwap() external view returns (uint8);
+
     function swap(uint amount, bytes32 token, bytes32 receiveToken, address recipient, uint receiveAmountMin) external;
 }
