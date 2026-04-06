24c6815122ec2e5f3feeda4fdf1c9c07104c07bf
Yaroslav Nekryach <yaroslavnekryach@gmail.com>
2026-04-06 14:51:42 +0300

    Refactored tests to use `await expect` for better assertion readability and consistency

--- test/auto-deposit-parent.test.ts
+++ test/auto-deposit-parent.test.ts
@@ -136,19 +136,20 @@ describe('AutoDepositParent', () => {
 
         it('Success: should collect payment', async () => {
           const feeTokenAmount = parseUnits('10', tokenPrecision);
-          const tx = await autoDepositParent
-            .connect(alice)
-            .createDepositWalletsBatch(
-              alice.address,
-              token.address,
-              minDepositAmount,
-              feeTokenAmount,
-              [CHAIN_2, CHAIN_3, CHAIN_4],
-              { value: '11000' },
-            );
-          expect(tx).to.changeTokenBalances(
-            autoDepositParent,
-            [alice, autoDepositParent.address],
+          await expect(() =>
+            autoDepositParent
+              .connect(alice)
+              .createDepositWalletsBatch(
+                alice.address,
+                token.address,
+                minDepositAmount,
+                feeTokenAmount,
+                [CHAIN_2, CHAIN_3, CHAIN_4],
+                { value: '11000' },
+              ),
+          ).to.changeTokenBalances(
+            token,
+            [alice, autoDepositParent],
             ['-' + feeTokenAmount.toString(), feeTokenAmount.toString()],
           );
         });
@@ -195,9 +196,9 @@ describe('AutoDepositParent', () => {
           });
 
           it('Success: should withdraw accumulated gas', async () => {
-            expect(
-              await autoDepositParent.withdrawGas(amount),
-            ).changeEtherBalances(
+            await expect(() =>
+              autoDepositParent.withdrawGas(amount),
+            ).to.changeEtherBalances(
               [owner, autoDepositParent],
               [amount, '-' + amount],
             );
@@ -218,9 +219,9 @@ describe('AutoDepositParent', () => {
           });
 
           it('Success: should withdraw accumulated tokens', async () => {
-            expect(
-              await autoDepositParent.withdraw(token.address),
-            ).changeTokenBalances(
+            await expect(() =>
+              autoDepositParent.withdraw(token.address),
+            ).to.changeTokenBalances(
               token,
               [owner, autoDepositParent],
               [tokenAmount, '-' + tokenAmount],
